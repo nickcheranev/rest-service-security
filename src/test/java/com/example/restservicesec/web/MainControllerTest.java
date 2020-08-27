67adda3ed5b205af1ab24731d9030a3bc6ceb69a
ncheranev <ncheranev@diasoft.ru>
2020-08-27 20:04:58 +0500

    add integration test

---
+++ src/test/java/com/example/restservicesec/web/MainControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.restservicesec.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Cheranev N.
+ * created on 27.08.2020.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MainControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port);
+    }
+
+    @Test
+    public void givenAuthRequestOnPrivateService_shouldSucceedWith200() {
+        ResponseEntity<String> result = template.withBasicAuth("guest", "password")
+                .getForEntity(base + "/index", String.class);
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+}
